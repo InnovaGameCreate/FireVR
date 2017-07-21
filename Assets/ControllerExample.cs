32ed6390bb21a6b74e183008d41aeef8ff1577e8
tugameproject <tugameproject@gmail.com>
2017-07-22 01:23:19 +0900

    左コントローラのトリガーによる放物線視認による瞬間移動　右コントローラに消火器追従　右コントローラトリガーで煙噴出(script仮)　

---
+++ Assets/ControllerExample.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class ControllerExample : MonoBehaviour
+{
+    public GameObject controller;
+    ParticleSystem smoke;           //煙パーティクルコンポーネント
+    private bool issmoking;         //煙出してるかどうか
+    private void Start()
+    {
+        smoke = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        SteamVR_TrackedObject trackedObject = controller.GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("トリガーを浅く引いた");
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("トリガーを深く引いた");
+            if (!issmoking)
+                smoke.Play();
+            issmoking = true;
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("トリガーを離した");
+            smoke.Stop();
+            issmoking = false;
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("タッチパッドをクリックした");
+        }
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("タッチパッドをクリックしている");
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("タッチパッドをクリックして離した");
+        }
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("タッチパッドに触った");
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("タッチパッドを離した");
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            Debug.Log("メニューボタンをクリックした");
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log("グリップボタンをクリックした");
+        }
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //Debug.Log("トリガーを浅く引いている");
+        }
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //Debug.Log("トリガーを深く引いている");
+        }
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            //Debug.Log("タッチパッドに触っている");
+        }
+    }
+}
---
+++ Assets/Scripts/VR関連/ObjMoveToHand.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjMoveToHand : MonoBehaviour {
+    public GameObject hand;             //つかむ手のオブジェクト
+    public Vector3 handoffset;          //ものをつかむ位置のずれ
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = hand.transform.position ;
+        transform.rotation = hand.transform.rotation;
+	}
+}
