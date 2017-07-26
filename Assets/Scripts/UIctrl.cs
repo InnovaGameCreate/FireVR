6e59f438e2aba2f6ec31a6b78c2a361011dba23b
yoksy-dot <c501506019@tokushima-u.ac.jp>
2017-07-26 20:48:43 +0900

    うまくいっていなかったようなので別の方法で実装

---
+++ Assets/Scripts/UIctrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIctrl : MonoBehaviour {
+    public GameObject camera;
+    public GameObject Point;
+    public float distance;
+    public float MoveSpeed;
+
+    //private Vector3 distance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //カメラの前に現れる
+        //this.transform.position = Point.transform.position;
+        this.transform.position = Vector3.Slerp(this.transform.position, Point.transform.position, MoveSpeed);
+        //カメラの方向を見る
+        this.transform.LookAt(camera.transform);
+    }
+}
