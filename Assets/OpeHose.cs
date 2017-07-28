50d7de73fc65a25e91d5bd893383955e4b50b4de
miki kazuo <c501506022@tokushima-u.ac.jp>
2017-07-28 23:31:55 +0900

    消火器導入

---
+++ Assets/OpeHose.cs
@@ -0,0 +1,34 @@
+﻿namespace VRTK.Examples
+{
+    using System.Collections;
+    using UnityEngine;
+    public class OpeHose : VRTK_InteractableObject
+    {
+        private Transform pos;
+        private VRTK_ControllerEvents controllerEvents;
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+                     controllerEvents = usingObject.GetComponent<VRTK_ControllerEvents>();
+         
+        }
+        //使用終了　トリガーを引いた
+        public override void StopUsing(VRTK_InteractUse usingObject)
+        {
+            controllerEvents = null;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (controllerEvents)
+            {
+                Debug.Log(base.trackPoint);
+            }
+            else
+            {
+               
+            }
+        }
+    }
+
+}
