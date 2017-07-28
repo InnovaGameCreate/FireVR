6dfff1db992059fa7c224e8e21c424164b7afb15
tugameproject <tugameproject@gmail.com>
2017-07-29 01:59:26 +0900

    消火器　ひも保留
    Signed-off-by: tugameproject <tugameproject@gmail.com>

--- Assets/OpeHose.cs
+++ Assets/OpeHose.cs
@@ -2,31 +2,50 @@
 {
     using System.Collections;
     using UnityEngine;
+    using VRTK.GrabAttachMechanics;
+    using VRTK.SecondaryControllerGrabActions;
+
     public class OpeHose : VRTK_InteractableObject
     {
-        private Transform pos;
+        private Vector3 pos,changepos;
         private VRTK_ControllerEvents controllerEvents;
+
         public override void StartUsing(VRTK_InteractUse usingObject)
         {
                      controllerEvents = usingObject.GetComponent<VRTK_ControllerEvents>();
-         
+                    pos = transform.localPosition;
         }
         //使用終了　トリガーを引いた
         public override void StopUsing(VRTK_InteractUse usingObject)
         {
+       
             controllerEvents = null;
         }
 
         protected override void Update()
         {
+
             base.Update();
+            if (transform.localPosition.y > -2)
+            { 
+                Destroy(this.GetComponent<VRTK_SwapControllerGrabAction>());
+                Destroy(this.GetComponent<ConfigurableJoint>());
+                Destroy(this.GetComponent<VRTK_SpringJointGrabAttach>());
+                Destroy(this.GetComponent<SpringJoint>());
+                Destroy(this.GetComponent<VRTK_FixedJointGrabAttach>());
+                GetComponent<OpeHose>().isGrabbable = true;
+            }
             if (controllerEvents)
             {
-                Debug.Log(base.trackPoint);
+    
             }
             else
             {
-               
+                if (this.GetComponent<ConfigurableJoint>() == null)
+                {
+                  
+
+                }
             }
         }
     }
