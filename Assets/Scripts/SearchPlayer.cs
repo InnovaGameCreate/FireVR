6275d846e6c3909c8dcdcd8ceb10ba6e470ab8f1
tugameproject <tugameproject@gmail.com>
2017-07-28 23:27:47 +0900

    横山:オブジェクトに近づいたときにUIが表示されるように

---
+++ Assets/Scripts/SearchPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchPlayer : MonoBehaviour {
+
+    private bool FoundPlayer;
+
+	// Use this for initialization
+	void Start () {
+        FoundPlayer = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.name == "[VRTK][AUTOGEN][BodyColliderContainer]")
+        {
+            FoundPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.name == "[VRTK][AUTOGEN][BodyColliderContainer]")
+        {
+            FoundPlayer = false;
+            Debug.Log(coll.gameObject.name);
+        }
+    }
+
+    //プレイヤーが範囲に入ってるどうか
+    public bool isSercchPlayer()
+    {
+        return FoundPlayer;
+    }
+}
--- Assets/Scripts/UIctrl.cs
+++ Assets/Scripts/UIctrl.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIctrl : MonoBehaviour {
+    public GameObject Search;
     public GameObject camera;
-    public GameObject Point;
-    public float distance;
+    public GameObject Point;//UI出現ポイント
+    //public float distance;
     public float MoveSpeed;
 
-    //private Vector3 distance;
+    private SearchPlayer _search;
+    private Canvas _canvas;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        _search = Search.GetComponent<SearchPlayer>();
+        _canvas = GetComponent<Canvas>();
+
+        _canvas.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        //カメラの前に現れる
-        //this.transform.position = Point.transform.position;
-        this.transform.position = Vector3.Slerp(this.transform.position, Point.transform.position, MoveSpeed);
-        //カメラの方向を見る
-        this.transform.LookAt(camera.transform);
+
+        if (_search.isSercchPlayer() ) {
+            _canvas.enabled = true;
+
+            //カメラの方向を見る
+            this.transform.LookAt(camera.transform);
+        }
+        else
+        {
+            _canvas.enabled = false;
+        }
     }
 }
