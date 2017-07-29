e77b342f20f91c78cb1c2a8388e149eca637adb2
tugameproject <tugameproject@gmail.com>
2017-07-29 16:31:38 +0900

    ホースアニメーション（後位置固定)

--- Assets/OpeHose.cs
+++ Assets/OpeHose.cs
@@ -7,34 +7,46 @@
 
     public class OpeHose : VRTK_InteractableObject
     {
-        private Vector3 pos,changepos;
         private VRTK_ControllerEvents controllerEvents;
+        public int section;
+        private Transform localtra;
+        Animation anim ;
 
         public override void StartUsing(VRTK_InteractUse usingObject)
-        {
-                     controllerEvents = usingObject.GetComponent<VRTK_ControllerEvents>();
-                    pos = transform.localPosition;
+        {          
+            controllerEvents = usingObject.GetComponent<VRTK_ControllerEvents>();
+          
         }
         //使用終了　トリガーを引いた
         public override void StopUsing(VRTK_InteractUse usingObject)
         {
-       
             controllerEvents = null;
+            Destroy(GetComponent<Rigidbody>());
         }
-
+        protected override void Awake()
+        {
+            base.Awake();
+            anim = gameObject.GetComponent<Animation>();
+        }
         protected override void Update()
         {
-
+        
             base.Update();
-            if (transform.localPosition.y > -2)
+           
+            if (transform.localPosition.y > -2 &&section ==0 )
             { 
                 Destroy(this.GetComponent<VRTK_SwapControllerGrabAction>());
                 Destroy(this.GetComponent<ConfigurableJoint>());
                 Destroy(this.GetComponent<VRTK_SpringJointGrabAttach>());
                 Destroy(this.GetComponent<SpringJoint>());
-                Destroy(this.GetComponent<VRTK_FixedJointGrabAttach>());
-                GetComponent<OpeHose>().isGrabbable = true;
+                section = 1;
+              
+                anim.Play();          
             }
+   
+         
+
+            GetComponent<OpeHose>().isGrabbable = true;
             if (controllerEvents)
             {
     
---
+++ Assets/StopPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Examples;
+
+public class StopPosition : MonoBehaviour {
+    private Animation anim ;
+    private OpeHose ope;
+    public Transform localtra;
+    // Use this for initialization
+    void Start () {
+       anim = gameObject.GetComponent<Animation>();
+        ope = GetComponent<OpeHose>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ope.section == 1 && anim["Hose"].normalizedTime > 1)
+        {
+            localtra = transform;
+            Destroy(anim);
+            Destroy(GetComponent<Rigidbody>());
+            ope.section = 2;
+        }
+        transform.localPosition = localtra.localPosition;
+        transform.localRotation = localtra.localRotation;
+    }
+}
