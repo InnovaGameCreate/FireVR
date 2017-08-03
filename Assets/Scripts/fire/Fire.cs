254c3bfa7bafb9d3ef6a0006796b77a6f0db3b86
tugameproject <tugameproject@gmail.com>
2017-08-03 14:36:52 +0900

    煙に当たって消える炎

---
+++ Assets/Scripts/fire/Fire.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+    public int hp = 10;       //炎耐久値
+    void OnParticleCollision(GameObject obj)
+    {
+      
+        //処理内容
+        //例）衝突したオブジェクトタグがenemyだった場合、オブジェクトを破壊する
+        if (obj.gameObject.tag == "Smoke")
+        {
+            hp--;
+            if(hp<=0)
+            Destroy(this.gameObject);
+        }
+    }
+}
