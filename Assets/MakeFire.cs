cad1ebffe7598365b6583d489df387292aa138cb
tugameproject <tugameproject@gmail.com>
2017-08-03 16:59:33 +0900

    燃え広がる炎プロトタイプ

---
+++ Assets/MakeFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeFire : MonoBehaviour {
+    private int count;      //着火までのカウンター
+    public GameObject fire;  //炎オブジェクト
+    private bool check;
+
+    private void Update()
+    {
+   
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Fire"))
+        {
+           
+            check = true;
+        }
+        if (!check) {
+            if (other.CompareTag("Wood"))
+            {
+                count++;
+            }
+            if (count > 60 * 7 )
+            {
+                Instantiate(fire, transform.position, transform.rotation);
+                check = true;
+            }
+        }
+    }
+}
