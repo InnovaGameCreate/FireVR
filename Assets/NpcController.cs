2df9f5eb9de59fadc8ab8765e529a489ebe2a429
miki kazuo <c501506022@tokushima-u.ac.jp>
2017-08-22 14:56:11 +0900

    Kキーで焦る人 モデルインポート

---
+++ Assets/NpcController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour {
+
+    private bool confused;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.K)&&!confused)
+        {
+            confused = true;
+            GetComponent<Animator>().SetLayerWeight(1, 1);
+            GetComponent<Animator>().SetTrigger("Confused");
+        }
+
+    }
+}
