3bbbb2fae78bff88871fc0d523f28d293f4b6750
miki kazuo <c501506022@tokushima-u.ac.jp>
2017-09-01 16:04:23 +0900

    Mega Toon FX Free Pack improt

--- Assets/Scripts/NPC/焦る人/NpcController.cs
+++ Assets/Scripts/NPC/焦る人/NpcController.cs
@@ -1,23 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NpcController : MonoBehaviour {
+public class NpcController : MonoBehaviour
+{
 
     private bool confused;
-	// Use this for initialization
-	void Start () {
-        
+    private bool exiting;
+    NavMeshAgent agent;     //ナビメッシュ格納
+    public Transform exit;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.K)&&!confused)
-        {
-            confused = true;
-            GetComponent<Animator>().SetLayerWeight(1, 1);
-            GetComponent<Animator>().SetTrigger("Confused");
-        }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (!confused)
+            {
+                confused = true;
+                GetComponent<Animator>().SetLayerWeight(1, 1);
+                GetComponent<Animator>().SetTrigger("Confused");
+            }
+            else
+            {
+                exiting = true;
+                GetComponent<Animator>().SetLayerWeight(1, 0);
+ 
+            }
+        }
+        
+        if (exiting)
+        {
+            agent.SetDestination(exit.position);
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                GetComponent<Animator>().SetTrigger("Idle");
+                
+               // GetComponent<Animator>().applyRootMotion = false;
+                exiting = false;
+            }
+        }
     }
 }
