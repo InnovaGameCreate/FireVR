8b2ac5ada58475ccd374fe18415fe479fe446ba0
tugameproject <tugameproject@gmail.com>
2017-09-08 14:55:47 +0900

    ブロックに触れることによる階層移動

--- Assets/TeleportFloor.cs
+++ Assets/TeleportFloor.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class TeleportFloor : MonoBehaviour {
     public GameObject tospace;
     public float moveheight;
+    private GameObject player;  //プレイヤー
+ 
+    private void Update()
+    {
+        if (player == null)
+            player = GameObject.Find("[VRTK][AUTOGEN][BodyColliderContainer]");
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        
+        if (other.gameObject.name == "[VRTK][AUTOGEN][BodyColliderContainer]")
         {
+            Debug.Log(other.gameObject.name);
             other.transform.position = new Vector3( tospace.transform.position.x,moveheight, tospace.transform.position.z);
             other.transform.rotation = tospace.transform.rotation;
         }
