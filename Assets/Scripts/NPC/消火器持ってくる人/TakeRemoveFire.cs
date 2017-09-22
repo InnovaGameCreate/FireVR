a9378fe76f772e78f9bd1180dfd3da6e7363e5f8
miki kazuo <c501506022@tokushima-u.ac.jp>
2017-09-23 02:55:59 +0900

    npcが一度触れた消火器は持ってこない・npcの手に触れるよう新消火器の子要素の位置を下げた・消火器を持ってきた後逃げる

--- Assets/Scripts/NPC/消火器持ってくる人/TakeRemoveFire.cs
+++ Assets/Scripts/NPC/消火器持ってくる人/TakeRemoveFire.cs
@@ -6,12 +6,13 @@ using UnityEngine.AI;
 public class TakeRemoveFire : MonoBehaviour
 {
     public Transform player;          //プレイヤー
+    public Transform exit;      //消火器を持ってきた後 逃げる場所
     NavMeshAgent agent;     //ナビメッシュ格納
     private Faze state = Faze.WAIT;    //消火器持ってくるアクション段階
     private GameObject[] tagobjs;      //マップ上すべての消火器obj
     private GameObject nearobj; //NPCから最も近い消火器
-    private bool toTakeRemoveFire = false;//消化器をもってこいと言われたフラグ
- 
+    private bool toTakeRemoveFire = true;//消化器をもってこいと言われたフラグ
+
     //待機→消火器に向かう→消火器もってくる→終了　
     enum Faze
     {
@@ -37,14 +38,19 @@ public class TakeRemoveFire : MonoBehaviour
             //消火器までの距離
             float distance = Vector3.Distance(transform.position, removefire.transform.position);
             //距離が短ければデータ更新 
-             if (mindistance == -1 || mindistance > distance && removefire.GetComponent<VRTK.Examples.FireRemove>().get_smokepercent() == 100) 
-            {
-                mindistance = distance;
-                nearobj = removefire;
-            }
+            if (removefire.GetComponent<VRTK.Examples.FireRemove>().get_smokepercent() == 100 && !removefire.GetComponent<VRTK.Examples.FireRemove>().npctaked)
+                if (mindistance == -1 || mindistance > distance)
+                {
+                    mindistance = distance;
+                    nearobj = removefire;
+                }
 
         }
+        if (mindistance != -1)
+        nearobj.GetComponent<VRTK.Examples.FireRemove>().npctaked = true;
+
         this.nearobj = nearobj;
+
     }
 
     //最も近い消火器を返す
@@ -58,36 +64,36 @@ public class TakeRemoveFire : MonoBehaviour
         switch (state)
         {
             case Faze.WAIT:
-               
-                if (toTakeRemoveFire )
+
+                if (toTakeRemoveFire)
                 {
                     nearRemoveFire();
                     //煙残量100%の消火器が見つからなければ持ってこない
                     if (get_nearobj() == null)
                         return;
-       
+
                     state = Faze.PRETAKE;
                     agent.SetDestination(get_nearobj().transform.position);     //消火器へ移動
                     GetComponent<Animator>().SetTrigger("Run");
                     GetComponent<AudioSource>().Play();
-                
+
                 }
                 break;
             case Faze.PRETAKE:
                 //ナビゲーション距離が一定以上になるとagent.remainingDistance が0になるため agent.remainingDistance != 0
                 if (agent.remainingDistance <= agent.stoppingDistance && agent.remainingDistance != 0)
                 {
-                 
+                    agent.SetDestination(player.position);       //プレイヤーへ移動
                     state = Faze.TAKE;
                     //消火器をNPCの子にして位置補正
                     get_nearobj().transform.parent = transform;
                     get_nearobj().transform.localPosition = new Vector3(0, 1.2f, 0.4f);
                     get_nearobj().transform.localRotation = Quaternion.Euler(0, 180, 0);
                     get_nearobj().GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-                    
-                    agent.stoppingDistance = 2;     //agent.stoppingDistance =1 だとプレイヤーと近いため2に更新
+
+                    agent.stoppingDistance = 3;     //agent.stoppingDistance =1 だとプレイヤーと近いため3に更新
                     GetComponent<IKControl>().ikActive = true;  //ik有効
-                    agent.SetDestination(player.position);       //プレイヤーへ移動
+               
                 }
                 break;
             case Faze.TAKE:
@@ -100,6 +106,8 @@ public class TakeRemoveFire : MonoBehaviour
                     GetComponent<AudioSource>().Stop();
                     get_nearobj().GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
                     GetComponent<IKControl>().ikActive = false; //ik無効
+
+                    StartCoroutine("GoExit");       //数秒待機して逃げるコールチン開始
                 }
                 //else
                 //{
@@ -107,11 +115,26 @@ public class TakeRemoveFire : MonoBehaviour
                 //}
                 break;
             case Faze.FINISH:
+                //逃げ先まで2ユニット未満の距離なら消える
+                if (Vector3.Distance(exit.position, transform.position) < 2)
+                    Destroy(this.gameObject);
                 break;
             default:
                 break;
         }
 
+    }
+
+    IEnumerator GoExit()
+    {
+        yield return new WaitForSeconds(0.5f);
+        get_nearobj().transform.parent = null;      //消火器との子の関係を解除
+
+        yield return new WaitForSeconds(2);
+        agent.stoppingDistance = 1;
+        agent.SetDestination(exit.position);       //出口へ移動
+        GetComponent<Animator>().SetTrigger("Run");
+        GetComponent<AudioSource>().Play();
     }
 
 }
