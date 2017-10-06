84c00af854a88260885659bfa3575d6023290566
tugameproject <tugameproject@gmail.com>
2017-10-06 21:13:35 +0900

    クリア時文字エフェクト表示・効果音再生

--- Assets/Scripts/Result/ResltChecker.cs
+++ Assets/Scripts/Result/ResltChecker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ResltChecker : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class ResltChecker : MonoBehaviour
     private float time; //時間経過
     public int resttime = 120; //シーン移動まで時間  ※デバッグ用
     private bool LastFlag;
+    public GameObject clearparticle;       //クリア時のパーティクル
+    public Transform player;       //プレイヤー
+    public GameObject clearui; //クリアUI
+    private AudioSource[] se = new AudioSource[2]; //se
     // Use this for initialization
     void Start()
     {
@@ -21,6 +26,8 @@ public class ResltChecker : MonoBehaviour
         sc_npc[0] = GameObject.FindGameObjectsWithTag("ScoreNPC");
         maxhp = lasthp = 100;
         LastFlag = false;
+        se = GetComponents<AudioSource>();
+     
     }
     private void Update()
     {
@@ -30,10 +37,11 @@ public class ResltChecker : MonoBehaviour
         }
         sc_fire[2] = GameObject.FindGameObjectsWithTag("Fire");/*正直、無駄やと思うby横山*/
 
-        if (time > resttime || sc_fire[2].Length == 0 )
+        if (time > resttime || sc_fire[2].Length == 0)
         {
+            StartCoroutine(Clear());
             finish();
-            Application.LoadLevel("Result");        //リザルトシーンへ
+     
             LastFlag = true;
         }
         else if (time != -1)
@@ -79,5 +87,57 @@ public class ResltChecker : MonoBehaviour
         {
             Debug.Log(tagname + "タグがついたオブジェクトはありません");
         }
+    }
+
+    //クリア
+    IEnumerator Clear()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject ui;
+        ui = Instantiate(clearui, player.position + player.forward * 2, player.rotation);
+        ui.transform.parent = player;
+
+        if (time > resttime)
+        {
+            ui.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "時間切れ";
+            se[1].Play();
+        }
+        else
+        {
+            se[0].Play();
+            ui.transform.FindChild("Text").gameObject.GetComponent<Text>().text = "消化完了";
+            GameObject[] particle = new GameObject[8];
+
+            Vector3[] particlepos = new Vector3[8];
+            for (int i = 0; i < particlepos.Length; i++)
+                particlepos[i] = player.position;
+            particlepos[0] += player.right;
+            particlepos[1] -= player.right;
+            particlepos[2] += player.up;
+            particlepos[3] -= player.up;
+
+            for (int i = 0; i < 4; i++)
+            {
+                particle[i] = Instantiate(clearparticle, particlepos[i], player.rotation);
+                particle[i].transform.parent = player;
+            }
+
+            yield return new WaitForSeconds(1f);
+            particlepos[4] += player.up;
+            particlepos[4] += player.right;
+            particlepos[5] += player.up;
+            particlepos[5] -= player.right;
+            particlepos[6] -= player.up;
+            particlepos[6] -= player.right;
+            particlepos[7] -= player.up;
+            particlepos[7] += player.right;
+            for (int i = 4; i < particlepos.Length; i++)
+            {
+                particle[i] = Instantiate(clearparticle, particlepos[i], player.rotation);
+                particle[i].transform.parent = player;
+            }
+        }
+        yield return new WaitForSeconds(5f);
+        Application.LoadLevel("Result");        //リザルトシーンへ
     }
 }
